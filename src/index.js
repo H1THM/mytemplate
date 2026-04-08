e70927585885c0a235c0d0d8f3c1fc364b9868fd
H1THM <h1thm.csw@gmail.com>
2026-04-08 18:28:32 +0300

    add comment

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './styles.css';
 
 //new repo setup commands:
 //git clone repo-url repo-new-name
-//npm install
+//npm install   (install dependinces found in package.json)
 //npm start    (webpacklive server)
+//npn run build (creates dist/ and bundles everything, last step)
